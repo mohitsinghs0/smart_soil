19429302eb02e359a8749fe8418444661b9ae81d
Mohit Singh <116932546+wtffmohits@users.noreply.github.com>
2026-03-22 11:20:10 +0530

    first commit

---
+++ app/src/main/java/com/example/smart_soil/activities/ResetPasswordActivity.java
@@ -0,0 +1,13 @@
+package com.example.smart_soil.activities;
+
+import android.os.Bundle;
+import com.example.smart_soil.R;
+
+public class ResetPasswordActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
